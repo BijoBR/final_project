233bb8c2e2cd354d80a03242899a321dbd7bc80c
BijoBR <143886059+BijoBR@users.noreply.github.com>
2023-10-12 17:07:22 +0530

    Add files via upload

---
+++ AABB_output_file.py
@@ -0,0 +1,248 @@
+# PS D:\Food ordering app> & C:/Users/User/AppData/Local/Programs/Python/Python311/python.exe "d:/Food ordering app/mainfile.py"
+
+# Enter 
+# 1.add_item 
+# 2 edit_item 
+# 3 view_item 
+# 4 remove_item 
+# 5 Exit 1
+# add operation
+# Enter the name: Chicken
+# Enter the quatity: 1
+# Enter the price: 400
+# Enyter the discount: 20
+# Enter the stck: 10
+# Enter
+# 1.add_item
+# 2 edit_item
+# 3 view_item
+# 4 remove_item
+# 5 Exit 1
+# add operation
+# Enter the name: Mutton
+# Enter the quatity: 1
+# Enter the price: 800
+# Enyter the discount: 50
+# Enter the stck: 10
+# Enter
+# 1.add_item
+# 2 edit_item
+# 3 view_item
+# 4 remove_item
+# 5 Exit 1
+# add operation
+# Enter the name: fish  
+# Enter the quatity: 1
+# Enter the price: 450
+# Enyter the discount: 20
+# Enter the stck: 10
+# Enter
+# 1.add_item
+# 2 edit_item
+# 3 view_item
+# 4 remove_item
+# 5 Exit 3
+# FoodId: 19353
+#  Name: Chicken
+#  quantity: 1.0
+#  Price: 400.0
+#  discount: 20.0
+#  stock: 10
+# FoodId: 15605
+#  Name: Mutton
+#  quantity: 1.0
+#  Price: 800.0
+#  discount: 50.0
+#  stock: 10
+# FoodId: 14585
+#  Name: fish
+#  quantity: 1.0
+#  Price: 450.0
+#  discount: 20.0
+#  stock: 10
+# Enter
+# 1.add_item
+# 2 edit_item
+# 3 view_item
+# 4 remove_item
+# 5 Exit 2
+# Enter the FooDId to be edited: 14585
+# Enter the name: Chicken burger
+# Enter the quatity: 1
+# Enter the price: 350
+# Enyter the discount: 10
+# Enter the stck: 10
+# This foodid is not found
+# Enter
+# 1.add_item
+# 2 edit_item
+# 3 view_item
+# 4 remove_item
+# 5 Exit 3
+# FoodId: 19353
+#  Name: Chicken
+#  quantity: 1.0
+#  Price: 400.0
+#  discount: 20.0
+#  stock: 10
+# FoodId: 15605
+#  Name: Mutton
+#  quantity: 1.0
+#  Price: 800.0
+#  discount: 50.0
+#  stock: 10
+# FoodId: 14585
+#  Name: Chicken burger
+#  quantity: 1.0
+#  Price: 350.0
+#  discount: 20.0
+#  stock: 10
+# Enter
+# 1.add_item
+# 2 edit_item
+# 3 view_item
+# 4 remove_item
+# 5 Exit 4
+# Enter the FoodId to be removed: 15605
+# removed
+# This FoodId is found and  removed
+# Enter
+# 1.add_item
+# 2 edit_item
+# 3 view_item
+# 4 remove_item
+# 5 Exit 3
+# FoodId: 19353
+#  Name: Chicken
+#  quantity: 1.0
+#  Price: 400.0
+#  discount: 20.0
+#  stock: 10
+# FoodId: 14585
+#  Name: Chicken burger
+#  quantity: 1.0
+#  Price: 350.0
+#  discount: 20.0
+#  stock: 10
+# Enter
+# 1.add_item
+# 2 edit_item
+# 3 view_item
+# 4 remove_item
+# 5 Exit
+
+# USER OUTPUT:
+
+
+# PS D:\Food ordering app> & C:/Users/User/AppData/Local/Programs/Python/Python311/python.exe "d:/Food ordering app/xmainfile.py"
+# Enter 
+# 1 for registering 
+# 2 forlogin 
+# 3 for login_admin 
+# 4 place order 
+# 5 order history 
+# 6 update profile 
+# 7 Exit 1
+# Login completed
+# Enter the name: Bijo
+# Enter the mobile number: 8072581543
+# Enter the Email Id: br.bijo7777@gmail.com
+# Enter the address: 13/38 chemmannu vilai veedu aluvilai pacode post
+# Enter the password: Jojo
+# Enter
+# 1 for registering
+# 2 forlogin
+# 3 for login_admin
+# 4 place order
+# 5 order history
+# 6 update profile
+# 7 Exit 2
+# Login details
+# Enter the user name (email Id)br.bijo7777@gmail.com
+# Enter the password: Jojo
+# User found and successfully loged in
+# Enter
+# 1 for registering
+# 2 forlogin
+# 3 for login_admin
+# 4 place order
+# 5 order history
+# 6 update profile
+# 7 Exit 3
+# Login through admin
+# Enter the email Id: don123@gmail.com
+# Enter the passsword: abcd@
+# Login sucessfully and details found
+# Enter
+# 1 for registering
+# 2 forlogin
+# 3 for login_admin
+# 4 place order
+# 5 order history
+# 6 update profile
+# 7 Exit 4
+# Enter the number of item to be ordered: 4
+# Enter
+# 1 for Tandoori Chicken (4 pieces) [INR 240]
+# 2 for Vegan Burger (1 Piece) [INR 320]
+# 3 for Truffle Cake (500gm) [INR 900] 1
+# Tandoori Chicken (4 pieces) [INR 240]
+# Your orders are : Tandoori Chicken (4 pieces) [INR 240]
+# Enter
+# 1 for Tandoori Chicken (4 pieces) [INR 240]
+# 2 for Vegan Burger (1 Piece) [INR 320]
+# 3 for Truffle Cake (500gm) [INR 900] 2
+# Vegan Burger (1 Piece) [INR 320]
+# Your orders are : Tandoori Chicken (4 pieces) [INR 240]
+# Your orders are : Vegan Burger (1 Piece) [INR 320]
+# Enter
+# 1 for Tandoori Chicken (4 pieces) [INR 240]
+# 2 for Vegan Burger (1 Piece) [INR 320]
+# 3 for Truffle Cake (500gm) [INR 900] 4
+# Item is not available
+# Your orders are : Tandoori Chicken (4 pieces) [INR 240]
+# Your orders are : Vegan Burger (1 Piece) [INR 320]
+# Enter
+# 1 for Tandoori Chicken (4 pieces) [INR 240]
+# 2 for Vegan Burger (1 Piece) [INR 320]
+# 3 for Truffle Cake (500gm) [INR 900] 2
+# Vegan Burger (1 Piece) [INR 320]
+# Your orders are : Tandoori Chicken (4 pieces) [INR 240]
+# Your orders are : Vegan Burger (1 Piece) [INR 320]
+# Your orders are : Vegan Burger (1 Piece) [INR 320]
+# Enter
+# 1 for registering
+# 2 forlogin
+# 3 for login_admin
+# 4 place order
+# 5 order history
+# 6 update profile
+# 7 Exit 5
+# order history
+# Your order history is:  Tandoori Chicken (4 pieces) [INR 240]
+# Your order history is:  Vegan Burger (1 Piece) [INR 320]
+# Your order history is:  Vegan Burger (1 Piece) [INR 320]
+# Enter
+# 1 for registering
+# 2 forlogin
+# 3 for login_admin
+# 4 place order
+# 5 order history
+# 6 update profile
+# 7 Exit 6
+# profile is updated
+# Enter the applicant mobile to update the profile: 8072581543
+# Enter the new name: Jhon
+# Enter the new Email Id: jhon123@gmail.com
+# Enter the new address: 1/Ab flat mumbai
+# Enter the new password: Koko
+# Mobile number not found
+# Enter
+# 1 for registering
+# 2 forlogin
+# 3 for login_admin
+# 4 place order
+# 5 order history
+# 6 update profile
+# 7 Exit 7
+# PS D:\Food ordering app> 
